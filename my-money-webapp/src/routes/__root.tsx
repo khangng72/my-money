e5fb6d6d949b1dd73b748b98a5024c282a1dec4f
khangng72 <khangnguyn07022002@gmail.com>
2026-04-09 21:13:56 +0700

    feat: add tanstack router

---
+++ my-money-webapp/src/routes/__root.tsx
@@ -0,0 +1,23 @@
+import { Link, Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+
+export const Route = createRootRoute({
+  component: RootComponent,
+  notFoundComponent: () => {
+    return (
+      <div>
+        <p>Not Found</p>
+        <Link to="/">Start Over</Link>
+      </div>
+    );
+  },
+});
+
+function RootComponent() {
+  return (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools position="bottom-right" />
+    </>
+  );
+}
